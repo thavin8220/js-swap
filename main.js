37bb196724492a006523f51d8fd7cb04d5ada7b1
thavin8220 <thavinmurugan@gmail.com>
2022-12-16 16:46:40 +0530

    add file

---
+++ main.js
@@ -0,0 +1,12 @@
+let a=prompt("Enter the value");
+let b=prompt("Enter the value");
+let c;
+function value(a,b){
+   c=a;
+   a=b;
+   b=c;
+   document.write(a +"<br>");
+   document.write(b);
+}
+value(a,b)
+
